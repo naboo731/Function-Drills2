cc8b13c148608d0c2735de77dcd53da008b4a520
Nichole Boothman <nbooth731@gmail.com>
2021-11-17 17:50:58 -0500

    Problem 1 complete

--- arrayMethods.js
+++ arrayMethods.js
@@ -21,6 +21,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 const evenNumbers = mixedNumbers.filter(function(num){
   return num % 2 === 0
 })
+console.log(evenNumbers)
 
 
 ////////// PROBLEM 2 //////////
@@ -40,7 +41,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+// const postTaxPrices // = prices.map(/* Provide Your Callback Here );
 
 
 
@@ -58,7 +59,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+// const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
 
 
 
@@ -83,7 +84,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+// const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
 
 
