183c20500d7b571595f4fb0333512d8055851d15
Nichole Boothman <nbooth731@gmail.com>
2021-11-17 16:24:42 -0500

    Problem 1 solved

--- callbackAndHigherOrder.js
+++ callbackAndHigherOrder.js
@@ -7,14 +7,17 @@
 
 // CODE HERE
 
+const multiply = (num1, num2, callback) => {
+  return callback(num1 * num2)
+}
 
 // UNCOMMENT THE FUNCTION CALL BELOW
 // RUN THIS FILE WITH NODE
 // CHECK YOUR ANSWER
 
-// multiply(4, 3, answer => {
-//   console.log('The answer is ' + answer) //should console.log 12
-// })
+multiply(4, 3, answer => {
+  console.log('The answer is ' + answer) //should console.log 12
+})
 
 
 
